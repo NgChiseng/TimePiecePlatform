5a5e683bc23ba1ac6d2ef579cff2b374c441d077
NgChiseng <ngchiseng@gmail.com>
2017-08-16 17:18:23 -0400

    TPW-01 #comment Lack to fix the admins registration.

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import url
 import django.contrib.auth.views
 from users.views import *
 
+# url(Path, Function or Class of the view, key to call each view in the html or view function)
 urlpatterns = [
     url(r'^login/$', log_in, name='login'),
 
-    url(r'^first_session/(?P<activationKey>\w+)$', FirstSession.as_view(), name='first_session'),
+    url(r'^first-session/(?P<activationKey>\w+)$', FirstSession.as_view(), name='first_session'),
 
     url(r'^administration', Administration.as_view(), name='administration'),
+
+    # url(r'^register-admin', RegisterAdmin.as_view(), name='register_admin'),
 ]
